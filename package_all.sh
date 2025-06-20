20593e77fecdb63071da65b2e03f75e7fd4052c1
Strain <errol.strain@cfm5158769.fda.gov>
2025-06-20 10:04:13 -0400

    Clean up package script

--- package_all.sh
+++ package_all.sh
@@ -1,53 +1,55 @@
-# /bin/sh
+#!/bin/bash
+set -e
 
-# This script MUST be run from within helm-charts directory
-# This script assumes the following directories exist:
-# ../cloudman-helm
-# ../cloudlaunch-helm
-# ../galaxy-helm
-# This script will package cloudlaunch, cloudlaunchserver, cloudman, and galaxy
-# and use the packaged version of each as a dependency for the next for the above charts
-# (i.e. one does not have to change the dependencies of each of the charts to point to their
-# fork, the final packaged cloudman will have the packaged cloudlaunch version, and not the one
-# downloaded from the dependency.yml)
+# Constants
+CHART_DIR="charts/galaxy"
+OUTPUT_DIR="charts"
+PUBLISH_BRANCH="gh-pages"
+WORKING_BRANCH="galaxy-5.19.0-custom"
+CHART_REPO_URL="https://estrain.github.io/galaxy-helm-custom"
 
-REPO_NAME=$(basename $(dirname $(git remote get-url origin)))
-BRANCH_NAME=$(git branch --show-current)
-# Packaged CLServer and CL charts are automatically added as the dependency charts for
-# CL and CM respectively
-echo Packaging as repo: $REPO_NAME, branch: $BRANCH_NAME
+echo "Checking out working branch: $WORKING_BRANCH"
+git checkout $WORKING_BRANCH
 
-rm -rf ../cloudlaunch-helm/cloudlaunchserver/charts
-rm -rf ../cloudlaunch-helm/cloudlaunch/charts
-rm -rf ../cloudman-helm/cloudman/charts
-rm -rf ../galaxy-helm/galaxy/charts
-rm -rf ../cloudlaunch-helm/cloudlaunchserver/tmpcharts
-rm -rf ../cloudlaunch-helm/cloudlaunch/tmpcharts
-rm -rf ../cloudman-helm/cloudman/tmpcharts
-rm -rf ../galaxy-helm/galaxy/tmpcharts
-cd ../cloudlaunch-helm/cloudlaunchserver
-helm dependency update
-cd ../../cloudman-helm/cloudman
-helm dependency update
-cd ../../helm-charts/charts
-echo "\nPackaging cloudlaunchserver!\n"
-helm package ../../cloudlaunch-helm/cloudlaunchserver/
-mkdir -p ../../cloudlaunch-helm/cloudlaunch/charts
-cp cloudlaunchserver-*.tgz ../../cloudlaunch-helm/cloudlaunch/charts/
-echo "\nPackaging cloudlaunch!\n"
-helm package ../../cloudlaunch-helm/cloudlaunch
-cp cloudlaunch-*.tgz ../../cloudman-helm/cloudman/charts/
-echo "\nPackaging cloudman!\n"
-helm package ../../cloudman-helm/cloudman/
-export CHARTS_DIR=$(pwd)
-cd ../../galaxy-helm/galaxy
-helm dependency update
-echo "\nPackaging galaxy!\n"
-sh scripts/helm_package $CHARTS_DIR
-cd ../../helm-charts/charts
-echo "\nPackaging Galaxy CVMFS-CSI!\n"
-helm package ../../galaxy-cvmfs-csi-helm/galaxy-cvmfs-csi/
-cd ..
-rm -f index.yaml
-echo "\nReindexing!\n"
-helm repo index . --url https://raw.githubusercontent.com/$REPO_NAME/helm-charts/$BRANCH_NAME/
+# Get version from Chart.yaml
+CHART_VERSION=$(grep '^version:' $CHART_DIR/Chart.yaml | awk '{print $2}')
+PACKAGE_NAME="galaxy-$CHART_VERSION.tgz"
+
+echo "Packaging chart version $CHART_VERSION"
+rm -f $OUTPUT_DIR/*.tgz
+helm package $CHART_DIR -d $OUTPUT_DIR
+
+echo "Rebuilding index.yaml"
+helm repo index $OUTPUT_DIR --url $CHART_REPO_URL
+
+# Save packaged files before switching branches
+TMP_DIR=$(mktemp -d)
+cp $OUTPUT_DIR/$PACKAGE_NAME $TMP_DIR/
+cp $OUTPUT_DIR/index.yaml $TMP_DIR/
+
+# Reset local changes so we can switch branches
+echo "Cleaning up local changes before switching branches"
+git restore --staged .
+git checkout -- .
+git clean -fd
+
+echo "Switching to publish branch: $PUBLISH_BRANCH"
+git checkout $PUBLISH_BRANCH
+
+echo "Removing old files from $PUBLISH_BRANCH"
+rm -f *.tgz index.yaml
+
+echo "Copying new files"
+cp $TMP_DIR/$PACKAGE_NAME .
+cp $TMP_DIR/index.yaml .
+
+echo "Committing and pushing to $PUBLISH_BRANCH"
+git add .
+git commit -m "Publish chart version $CHART_VERSION"
+git push origin $PUBLISH_BRANCH
+
+echo "Done. Chart version $CHART_VERSION published to GitHub Pages."
+
+# Clean up
+rm -rf $TMP_DIR
+
